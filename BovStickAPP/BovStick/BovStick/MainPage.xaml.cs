8241377cb8cfc0194a0db54ac636038a04ab1be5
Vanderlansantos <vanderlan_go2010@hotmail.com>
2018-06-17 09:38:37 -0300

    Implementacao dos verbos http - Inicio do dev mobile

---
+++ BovStickAPP/BovStick/BovStick/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BovStick
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            BtnPasto.Clicked += BtnPasto_Clicked;
+		}
+
+        private async void BtnPasto_Clicked(object sender, EventArgs e)
+        {
+           await  Navigation.PushAsync(new Pasto());
+        }
+    }
+}
